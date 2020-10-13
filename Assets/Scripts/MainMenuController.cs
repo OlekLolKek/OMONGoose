5f76be5dd42dfdbb56313c202f4f6af3179da1d0
NEDNAR <drakozavrov20@gmail.com>
2020-10-13 18:59:17 +0300

    Main Menu

---
+++ Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+
+public class MainMenuController : MonoBehaviour
+{
+    #region Fields
+
+    [SerializeField] private GameObject _camera;
+    [SerializeField] private float _rotateSpeed;
+
+    #endregion
+
+
+    #region UnityMethods
+
+    private void Update()
+    {
+        _camera.transform.Rotate(Vector3.up, _rotateSpeed * Time.deltaTime);
+    }
+
+    #endregion
+}
